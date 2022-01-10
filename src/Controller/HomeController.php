e9200a9f8ddf1625d6cdd32ca0173e7c4fbc8272
Jerome Onadja <jeromeonadja28@gmail.com>
2022-01-10 15:34:33 +0000

    feat(api): update api doc

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,32 +3,36 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function sprintf;
 
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'businessman_api')]
     #[Route('/api/v1/', name: 'businessman_api_v1_doc')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         return $this->json([
-            'message' => 'Businessman payment api',
+            'message' => 'My custom serveur payment api',
             'version' => 'v1',
             'documentation' => [
-                'request' => [
-                    'url' => 'https://dc73-102-67-113-184.ngrok.io/api/v1/pay',
-                    'method' => 'POST',
-                    'headers' => [
-                        'Content-type' => 'application/json',
-                        'X-AUTH' => 'JWT TOKEN'
-                    ],
-                    'body' => [
-                        "client_number" => "00000000",
-                        "payment_amount" => "100",
-                        "otp" => "123456",
-                    ],
+                'version' => 'v1',
+                'make_payment_request' => [
+                    'with_fetch_or_axios' => [
+                        'url' => sprintf("%s/api/v1/pay", $request->getSchemeAndHttpHost()),
+                        'method' => 'POST',
+                        'headers' => [
+                            'Content-type' => 'application/json',
+                        ],
+                        'body_or_data' => [
+                            "client_number" => "00000000",
+                            "payment_amount" => "100",
+                            "otp" => "123456",
+                        ],
+                    ]
                 ]
             ],
         ]);
