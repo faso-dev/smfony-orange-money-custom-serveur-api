189fcaa80683e54d4a5a7369545405ad8dfb6b61
Jerome Onadja <jeromeonadja28@gmail.com>
2022-06-20 15:13:36 +0000

    chore(dashboard): setup dashboard config

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,7 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Lopi\Bundle\PusherBundle\LopiPusherBundle::class => ['all' => true],
 ];
